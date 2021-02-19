172d5eb37966c43557815566255d00b0422709f9
odin thomas rochmann <odin.rochmann@gmail.com>
2021-02-19 12:46:22 +0100

    updated 📑

---
+++ docs/15-f750e0fb07a20a5dbef2.js
@@ -0,0 +1,34 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{85:function(e,a,n){"use strict";n.r(a),n.d(a,"render",(function(){return o}));var i=n(0),r=n.p+"e5bca237a7574829741bc02e5101b68a.jpg",s=n.p+"6e3bb232c21f871f8d8ca630a4f7e81c.png",c=n.p+"cf2a81716c546fba8db8a4a6d430c811.png";const o=i.e`
+    <cwc-learn-page>
+        <style>
+            
+            .links span{
+                display: flex;
+                align-items: center;
+                margin: 20px;
+            }
+            .links img{
+                width: 30px;
+                margin-right: 20px;
+            }
+
+            .links a{
+                text-decoration: none;
+                color: cornflowerblue;
+            }
+        </style>
+        <div slot="header">
+            <img src="${r}" />
+        </div>
+        <div class="links">
+            <span>
+                <img src="${s}" />   
+                <a href="https://odinr.github.io/learn-code-observable/">https://odinr.github.io/learn-code-observable/</a>
+            </span>
+            <span>
+                <img src="${c}" />   
+                <a href="https://github.com/odinr/learn-code-observable">https://github.com/odinr/learn-code-observable</a>
+            </span>
+        </div>
+    </cwc-learn-page>
+`;a.default=o}}]);
